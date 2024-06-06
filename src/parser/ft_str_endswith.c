46b4f45d191fd6d7e11050bf41d7f146eb8a4d09
Kwok-Po Chu <kwchu@f1r6s9.codam.nl>
2024-06-06 14:05:11 +0200

    fixed wrong safety check order for ft_str_endswith

--- src/parser/ft_str_endswith.c
+++ src/parser/ft_str_endswith.c
@@ -6,10 +6,10 @@ char	*ft_str_endswith(const char *s, const char *end)
 	int	len;
 	int	i;
 
-	if (s == end)
-		return ((char *)s);
 	if (!s || !end)
 		return (NULL);
+	if (s == end)
+		return ((char *)s);
 	len = ft_strlen(end) - 1;
 	i = ft_strlen(s) - 1;
 	if (len < 0 || i < 0 || i < len)
